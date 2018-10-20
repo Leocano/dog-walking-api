348592801ba91c9ad678890126adaec252a7e31f
Leonardo Cano <leocano4242@gmail.com>
2018-10-20 20:57:49 -0300

    Asserting every DogWalking has a pet

--- test/models/dog_walking_test.rb
+++ test/models/dog_walking_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class DogWalkingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save dog walking without a pet' do
+    dog_walking = DogWalking.new
+    assert_not dog_walking.save, 'Saved dog walking without a pet'
+  end
 end
