3f8b98ec91cf62c844737fe052ded8912d688ada
Leonardo Cano <leocano4242@gmail.com>
2018-10-20 20:09:41 -0300

    Adding relations between models

--- app/models/dog_walking.rb
+++ app/models/dog_walking.rb
@@ -1,3 +1,4 @@
 class DogWalking < ApplicationRecord
   belongs_to :dog_walking_status
+  has_and_belongs_to_many :pets
 end
--- app/models/dog_walking_status.rb
+++ app/models/dog_walking_status.rb
@@ -1,2 +1,3 @@
 class DogWalkingStatus < ApplicationRecord
+  has_many :dog_walkings
 end
