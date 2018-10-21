95d25d76a380b88d656aae9f8354d527df762c85
Leonardo Cano <leocano4242@gmail.com>
2018-10-20 22:00:40 -0300

    Removing a useless test

--- test/models/dog_walking_test.rb
+++ test/models/dog_walking_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class DogWalkingTest < ActiveSupport::TestCase
-  test 'should not save dog walking without a pet' do
-    dog_walking = DogWalking.new
-    assert_not dog_walking.save, 'Saved dog walking without a pet'
-  end
 end
