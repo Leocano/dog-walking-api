68dfdf1f43a968afe44b3e10049fbcbf07680054
Leonardo Cano <leocano4242@gmail.com>
2018-10-31 22:40:53 -0300

    Testing nil return on real_duration

--- test/models/dog_walking_test.rb
+++ test/models/dog_walking_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class DogWalkingTest < ActiveSupport::TestCase
+  test 'should return nil for real duration without finish_time' do
+    dog_walking = DogWalking.new
+    assert_nil dog_walking.real_duration
+  end
 end
